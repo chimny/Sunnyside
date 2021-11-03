b592f5ae5234fc065a7181e81c33e1060454c89f
chimny <aleks713@gmail.com>
2021-11-03 16:04:17 +0100

    working on button handlers for slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+//mobile navigation
+
 const burgerButton = document.querySelector(".burger");
 const ul = document.querySelector("ul");
 
@@ -6,3 +8,33 @@ const menuTrigger = () => {
 };
 
 burgerButton.addEventListener("click", menuTrigger);
+
+//testimonial slider
+
+const testimonials = document.querySelectorAll('.testimonial')
+
+const time = 3000;
+let activeIndex = 0;
+
+
+
+const changeTestimonial = ()=>{
+
+testimonials.forEach(testimonial =>{
+    testimonial === testimonials[activeIndex] ? testimonial.classList.add('active') : testimonial.classList.remove('active')
+})
+    activeIndex++;
+
+
+  if(activeIndex=== testimonials.length){
+      activeIndex= 0
+  }
+}
+
+
+
+
+setInterval(changeTestimonial, time)
+
+
+
