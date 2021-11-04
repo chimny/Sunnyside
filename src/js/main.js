e3bb573736048380cc7a5ae2cbc8677f4e624ae3
chimny <aleks713@gmail.com>
2021-11-04 13:21:52 +0100

    working slider with arrow hanlder and time delays

--- src/js/main.js
+++ src/js/main.js
@@ -12,39 +12,53 @@ burgerButton.addEventListener("click", menuTrigger);
 //testimonial slider
 
 const testimonials = document.querySelectorAll('.testimonial')
-
-//initial setup
-const time = 2000;
-let activeIndex = 0;
-
-const changeTestimonial = ()=>{
-
-testimonials.forEach(testimonial =>{
-    testimonial === testimonials[activeIndex] ? testimonial.classList.add('active') : testimonial.classList.remove('active')
-})
-    activeIndex++;
-  if(activeIndex=== testimonials.length){
-      activeIndex= 0
-  }
-}
-
 const leftArrow = document.querySelector('.left-arrow');
 const rightArrow = document.querySelector('.right-arrow');
 
-//@handling buttons to dynamicly change testimonials and clear and setInterval
+//interface
+const time = 4000;
+let activeIndex = 0;
 
-leftArrow.addEventListener('click',()=>{
-    console.log('kliknięto w lewy przycisk');
-})
-
-rightArrow.addEventListener('click',()=>{
-    activeIndex++;
-    if(activeIndex=== testimonials.length){
-        activeIndex= 0
+//function responsible for changing testimonials. Backwards and forward instructions included
+const changeTestimonial = (direction) => {
+    testimonials.forEach(testimonial => {
+        testimonial === testimonials[activeIndex] ? testimonial.classList.add('active') : testimonial.classList.remove('active')
+    })
+    switch (direction) {
+        case 'forward':
+            if (activeIndex === testimonials.length-1) {
+                activeIndex = 0
+            } else {
+                activeIndex++;
+            }
+            break;
+        case 'backwards':
+            if (activeIndex === 0) {
+                activeIndex = testimonials.length - 1;
+            } else {
+                activeIndex--;
+            }
+            break;
+        default:
+            return
     }
-})
+}
 
-setInterval(changeTestimonial, time)
+let testimonialInterval = setInterval(() => changeTestimonial('forward'), time)
+
+const arrowHandler =(direction) => {
+    changeTestimonial(direction)
+    clearInterval(testimonialInterval);
+    setTimeout(() => {
+        clearInterval(testimonialInterval);
+        testimonialInterval = setInterval(()=>changeTestimonial('forward'), time)
+    }, time)
+}
+
+leftArrow.addEventListener('click', ()=> arrowHandler('backwards') )
+rightArrow.addEventListener('click', ()=> arrowHandler('forward') )
+
+
 
 
 
